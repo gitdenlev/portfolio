6e3a7a3483f4287ef309f83672339c6277bdfcd7
Denys <levchenkoden02@gmail.com>
2023-12-21 00:19:37 +0200

    Init project

---
+++ src/data/constants.js
@@ -0,0 +1,43 @@
+export const Bio = {
+  name: "Denys Levchenko",
+  role: "Frontend Developer",
+  description:
+    "I am a beginner Frontend Developer, a continuous learner always eager to expand my knowledge and explore new horizons. I embrace challenges and thrive in environments that foster growth and development. I am passionate about learning and helping others discover their own potential",
+  github: "https://github.com/gitdenlev",
+  instagram: "https",
+};
+
+export const Skills = [
+  {
+    title: "HTML",
+    img: "http://dssd'",
+  },
+  {
+    title: "CSS",
+    img: "http://dssd'",
+  },
+  {
+    title: "SCSS",
+    img: "http://dssd'",
+  },
+  {
+    title: "Javascript",
+    img: "http://dssd'",
+  },
+  {
+    title: "React",
+    img: "http://dssd'",
+  },
+  {
+    title: "Git",
+    img: "http://dssd'",
+  },
+  {
+    title: "Node",
+    img: "http://dssd'",
+  },
+  {
+    title: "Github",
+    img: "http://dssd'",
+  },
+];
